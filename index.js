c9162cc98dfcf4ce24a63804a3ee9838bd0f7156
sebug <sebastian.gfeller@gmail.com>
2018-05-19 11:45:18 +0200

    Copying over files

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-if (process.argv.length < 3) {
-    console.error('Usage: require-parallel-optimizer source-directory target-directory');
+if (process.argv.length < 4) {
+    console.error('Usage: node require-parallel-optimizer source-directory target-directory');
     process.exitCode = 1;
     return;
 }
 
+const fs = require('fs-extra');
+
+function prepare(sourceDirectory, targetDirectory) {
+    return fs.copy(sourceDirectory, targetDirectory);
+}
+
+function optimize(sourceDirectory, targetDirectory) {
+    fs.copy(sourceDirectory, targetDirectory).then(function () {
+	console.log('copied over');
+    });
+}
+
+optimize(process.argv[2], process.argv[3]);
 
