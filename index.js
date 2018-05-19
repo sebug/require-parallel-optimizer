eaced6a7e41825c6d4f2851a186e367185311c56
sebug <sebastian.gfeller@gmail.com>
2018-05-19 15:57:10 +0200

    Introducing stub for optimization

--- index.js
+++ index.js
@@ -8,20 +8,42 @@ const fs = require('fs-extra');
 const path = require('path');
 const adaptRequireConfig = require('./adapt_require_config');
 
+function runOptimizer(rJSPath, buildConfigFile) {
+    return new Promise((resolve, reject) => {
+	console.log('running ' + rJSPath + ' -o ' + buildConfigFile);
+	resolve(buildConfigFile);
+    });
+}
+
 async function optimize(sourceDirectory, targetDirectory, requireConfigName) {
     let rjsPath = path.join(path.dirname(process.argv[1]), 'node_modules', '.bin', 'r.js');
     const mainInstance = path.join(targetDirectory, 'main');
+
+    let existsAlready = await fs.pathExists(mainInstance);
+
+    if (existsAlready) {
+	await fs.remove(mainInstance);
+    }
+    
     await fs.copy(sourceDirectory, mainInstance);
 
     let numberOfSlices = 4;
 
     let adaptedRequireFiles = await adaptRequireConfig(path.join(mainInstance, requireConfigName), numberOfSlices);
 
+    let configFilePaths = [];
     for (let i = 0; i < adaptedRequireFiles.length; i += 1) {
 	const adaptedPath = path.join(mainInstance, 'adapted_' + i +
 				      '_' + requireConfigName);
 	await fs.writeFile(adaptedPath, adaptedRequireFiles[i], 'utf8');
+	configFilePaths.push(adaptedPath);
     }
+
+    const optimizationPromises = configFilePaths.map(fp => runOptimizer(rjsPath, fp));
+
+    await Promise.all(optimizationPromises);
+
+    console.log('After optimization. Now we will have to copy over files excluded but necessary.');
 }
 
 optimize(process.argv[2], process.argv[3], process.argv[4]);
