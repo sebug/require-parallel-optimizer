f142de13301c4fd7e650f9ce25030fe3ddf10f50
sebug <sebastian.gfeller@gmail.com>
2018-05-19 13:05:26 +0200

    Respecting require.build.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-if (process.argv.length < 4) {
-    console.error('Usage: node require-parallel-optimizer source-directory target-directory');
+if (process.argv.length < 5) {
+    console.error('Usage: node require-parallel-optimizer source-directory target-directory require.build.config');
     process.exitCode = 1;
     return;
 }
 
 const fs = require('fs-extra');
+const path = require('path');
 
 function prepare(sourceDirectory, targetDirectory) {
     return fs.copy(sourceDirectory, targetDirectory);
 }
 
-async function optimize(sourceDirectory, targetDirectory) {
-    await fs.copy(sourceDirectory, targetDirectory);
+async function optimize(sourceDirectory, targetDirectory, requireConfigName) {
+    const mainInstance = path.join(targetDirectory, 'main');
+    await fs.copy(sourceDirectory, mainInstance);
     console.log('copied over');
 }
 
-optimize(process.argv[2], process.argv[3]);
+optimize(process.argv[2], process.argv[3], process.argv[4]);
 
